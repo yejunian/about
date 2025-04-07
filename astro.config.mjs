28b1953554637d1335c9c9fad8e5520f9b27c1cc
yejunian <leeye51456@gmail.com>
2025-04-07 23:52:26 +0900

    📦️ deps: `astro-icon` 통합 추가
    Tabler 아이콘 세트를 사용하기 위해 `@iconify-json/tabler`도 함께 추가.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://yejunian.github.io",
   base: "/about",
   trailingSlash: "always",
+
+  integrations: [icon()],
 });
